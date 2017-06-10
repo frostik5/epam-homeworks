703f89c7de5fe36635bb5b0e293779c0f7f23564
Stanislav <rstason@gmail.com>
2017-06-10 21:33:32 +0300

    Tasks 1 of Unit05

---
+++ src/main/java/se05/task01/AppStart.java
@@ -0,0 +1,110 @@
+package se05.task01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import static se05.task01.SystemExplorer.*;
+
+public class AppStart {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+       executeUserDialogue();
+    }
+
+    private static void executeUserDialogue() {
+        getCurrentDir();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String line;
+            while (true) {
+                if ((line = reader.readLine()) != null) {
+                    String[] words = line.split("\\s+");
+                    String command = words[0].toLowerCase();
+                    StringBuilder fileSB;
+
+                    if (words.length == 1) {
+                        switch (command) {
+                            case "dir":  dir(); break;
+                            case "cd..": cd(".."); break;
+                            case "help":
+                            case "?":    showHelp(); break;
+                            case "exit":
+                            case "quit": System.exit(0); break;
+                        }
+                    } else if (words.length > 1) {
+                        fileSB = new StringBuilder();
+                        for (int i = 1; i < words.length; i++) {
+                            if (i < words.length - 1) {
+                                fileSB.append(words[i]);
+                                fileSB.append(" ");
+                            } else {
+                                fileSB.append(words[i]);
+                            }
+                        }
+                        String file = fileSB.toString();
+                        String text;
+
+                        switch (command) {
+                            case "cd":      cd(file); break;
+                            case "mkdir":   mkDir(file); break;
+                            case "create":  create(file); break;
+                            case "delete":  delete(file); break;
+                            case "open":    open(file); break;
+                            case "setdir":  setCurrentDir(words[1]); break;
+                            case "rewrite": text = typeText(reader); rewrite(file, text); break;
+                            case "addto":   text = typeText(reader); addTo(file, text); break;
+                            default:        System.err.println("--> Incorrect command! Try \"help\" or \"?\" to watch the full list of supporterd commands."); break;
+                        }
+                    } else {
+                        System.err.print("Input error! Please try again: ");
+                    }
+                } else {
+                    System.err.print("Input error! Please try again: ");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String typeText(BufferedReader reader) {
+        String text;
+        StringBuilder textSB = new StringBuilder();
+        System.out.println("=================================================================================================================================");
+        System.out.println("                              PLEASE ENTER YOUR TEXT HERE (type \"end\" on a new line to stop)");
+        System.out.println("=================================================================================================================================");
+        try {
+            while (true) {
+                if ("end".equals(text = reader.readLine())) break;
+                else  {
+                    textSB.append(text);
+                    textSB.append(System.lineSeparator());
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("--> Input error!");
+        }
+        System.out.println("=================================================================================================================================");
+        return textSB.toString();
+    }
+
+    private static void showHelp() {
+        System.out.println("=================================================================================================================================");
+        System.out.println("                                              LIST OF ALL SUPPORTED COMMANDS");
+        System.out.println("=================================================================================================================================");
+        System.out.println("dir                          - shows list of all files and folders of current directory;");
+        System.out.println("cd [folder_name]             - goes to folder [folder_name] from current directory, use command \"..\" or \"cd..\" to go up one level;");
+        System.out.println("mkdir [folder_name]          - creates a folder [folder_name] in current directory;");
+        System.out.println("create [file_name.extension] - creates a [file_name.extension] file in current directory;");
+        System.out.println("delete [file_name.extension] - deletes a [file_name.extension] file from current directory;");
+        System.out.println("setdir [directory]           - sets current directory to [directory];");
+        System.out.println("open [file_name.extension]   - opens file [file_name.extension] from current directory;");
+        System.out.println("rewrite [file_name]          - rewrites a .txt [file_name] with the text you enter;");
+        System.out.println("addto [file_name]            - adds to a .txt [file_name] text you enter;");
+        System.out.println("help or ?                    - show list of all supported commands;");
+        System.out.println("exit or quit                 - exit the program.");
+        System.out.println("=================================================================================================================================");
+        getCurrentDir();
+    }
+}
---
+++ src/main/java/se05/task01/SystemExplorer.java
@@ -0,0 +1,205 @@
+package se05.task01;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.*;
+
+/**
+ * Разработать приложение, позволяющее просматривать файлы и каталоги файловой системы, а также создавать и удалять текстовые файлы.
+ * Для работы с текстовыми файлами необходимо реализовать функциональность записи (дозаписи) в файл.
+ * Требуется определить исключения для каждого слоя приложения и корректно их обработать.
+ */
+public class SystemExplorer {
+    private static final String OS = System.getProperty("os.name");
+    private static final File root = new File(OS.contains("windows") ? "C:/" : "/");
+    private static final String sep = File.separator;
+    private static File currentDir = new File(root.getAbsolutePath());
+
+    public static void cd(String folder) {
+        File tmp = new File(currentDir.getAbsolutePath()).getParentFile();
+        if (folder.equals("..") && tmp != null) {
+            currentDir = tmp;
+        } else if (folder.equals("..") && tmp == null) {
+            System.err.println("--> You are in a root folder!");
+        } else {
+            tmp = new File(currentDir + sep + folder);
+            if (tmp.exists() && tmp.isDirectory()) {
+                currentDir = tmp;
+            } else {
+                try {
+                    throw new FileNotFoundException(tmp.getAbsolutePath());
+                } catch (FileNotFoundException e) {
+                    System.err.println("--> Folder not found!");
+                }
+            }
+        }
+        getCurrentDir();
+    }
+
+    // Необходимо добавить ортировку по файлам и папкам
+    public static void dir() throws UnsupportedEncodingException {
+        List<File> files;
+        files = new ArrayList<>(Arrays.asList(currentDir.listFiles()));
+
+        if (files.size() == 0) {
+            System.out.println("--> Folder is empty!");
+            getCurrentDir();
+            return;
+        }
+
+        for (File f : files) {
+            if (f.isFile()) {
+                System.out.println("File: " + f.getName() + "\t\t" + f.length() + " bytes");
+            } else {
+                System.out.println("Dir: " + f.getName());
+            }
+        }
+        System.out.println(System.lineSeparator() + "Total items: " + files.size() );
+        getCurrentDir();
+    }
+
+    public static void mkDir(String name) {
+        if (containsIllegalChars(name, true)) {
+            getCurrentDir();
+            return;
+        }
+        File dir = new File(currentDir.getAbsolutePath() + "/" + name);
+        if (dir.exists()) {
+            try {
+                throw new FileAlreadyExistsException(dir.getAbsolutePath());
+            } catch (FileAlreadyExistsException e) {
+                System.err.println("--> Folder \"" + name + "\" already exists!");
+            }
+        } else {
+            dir.mkdir();
+            System.out.println("--> Folder \"" + name + "\" created!");
+        }
+        getCurrentDir();
+    }
+
+    public static void create(String name) {
+        if (containsIllegalChars(name, false)) {
+            getCurrentDir();
+            return;
+        }
+        File file = new File(currentDir.getAbsolutePath() + sep + name);
+        try {
+            if (file.exists() && file.isFile()) {
+                throw new FileAlreadyExistsException(file.getName());
+            } else if (name.matches("[\\w]+(\\.[\\w]+)+")) {
+                file.createNewFile();
+                System.out.println("--> File \"" + name + "\" created!");
+            } else {
+                System.err.println("--> Wrong file name input!");
+            }
+        } catch (FileAlreadyExistsException e) {
+            System.err.println("--> File \"" + name + "\" already exists!");
+        } catch (IOException e) {
+            System.err.println("--> Couldn't create file \"" + name + "\"! Probably you don't have administrator rights!");
+        }
+        getCurrentDir();
+    }
+
+    private static boolean containsIllegalChars(String name, boolean dir) {
+        String[] illegalChars = {"\\", "/", ":", "*", "?", "\"", "<", ">", "|"};
+        for (String ch : illegalChars) {
+            if (name.contains(ch)) {
+                if (dir)    System.err.println("--> Folder name should not contains the following symbols: \\ / : * ? \" < > |");
+                else        System.err.println("--> File name should not contains the following symbols: \\ / : * ? \" < > |");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void delete(String name) {
+        if (containsIllegalChars(name, false)) return;
+        File file = new File(currentDir.getAbsolutePath() + sep + name);
+        if (file.exists() && file.isFile()) {
+            file.delete();
+            System.out.println("--> File \"" + name + "\" deleted!");
+        } else if (file.exists() && file.isDirectory()) {
+            file.delete();
+            System.out.println("--> Folder \"" + name + "\" deleted!");
+        } else {
+            try {
+                throw new FileNotFoundException(file.getName());
+            } catch (FileNotFoundException e) {
+                System.err.println("--> File/folder " + name + " not found!");
+            }
+        }
+        getCurrentDir();
+    }
+
+    public static void rewrite(String name, String text) {
+        writeTo(name, text, true);
+    }
+
+    public static void addTo(String name, String text) {
+        writeTo(name, text, false);
+    }
+
+    private static void writeTo(String name, String text, boolean rewrite) {
+        File file = new File(currentDir.getAbsolutePath() + sep + name);
+        if (file.exists() && file.isFile()) {
+            if (rewrite) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                    writer.write(text);
+                } catch (IOException e) {
+                    System.err.println("--> Couldn't rewrite file \"" + name + "\"!");
+                }
+            } else {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(file, true))) {
+                    writer.write(text);
+                } catch (IOException e) {
+                    System.err.println("--> Couldn't add text to file \"" + name + "\"!");
+                }
+            }
+        }
+        getCurrentDir();
+    }
+
+    public static void open(String name) {
+        File file = new File(currentDir.getAbsolutePath() + sep + name);
+        if (file.exists() && file.isFile()) {
+            StringBuilder sb = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                while(reader.ready()) {
+                    sb.append(reader.readLine());
+                    sb.append(System.lineSeparator());
+                }
+                if (sb.toString().length() == 0) {
+                    System.out.println("--> File \"" + name + "\" is empty!");
+                } else {
+                    System.out.println("=================================================================================================================================");
+                    System.out.println("                                               " + name.toUpperCase() + " CONTENT");
+                    System.out.println("=================================================================================================================================");
+                    System.out.println(sb.toString());
+                    System.out.println("=================================================================================================================================");
+                }
+            } catch (IOException e) {
+                System.err.println("--> Couldn't open file \"" + name + "\"!");
+            }
+        }
+        getCurrentDir();
+    }
+
+    public static void getCurrentDir() {
+        System.out.print(currentDir + "> ");
+    }
+
+    public static void setCurrentDir(String currentDir) {
+        File tmp = new File(currentDir);
+        if (tmp.exists() && tmp.isDirectory()) {
+            SystemExplorer.currentDir = tmp;
+        } else {
+            try {
+                throw new FileNotFoundException(tmp.getAbsolutePath());
+            } catch (FileNotFoundException e) {
+                System.err.println("--> Directory not found!");
+            }
+        }
+        getCurrentDir();
+    }
+}
