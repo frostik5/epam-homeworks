9e3ad5d48c3b451733ea9448336cefbd9a30892a
Stanislav <rstason@gmail.com>
2017-06-10 22:19:11 +0300

    Tasks 1 of Unit05.

--- src/main/java/se05/task01/AppStart.java
+++ src/main/java/se05/task01/AppStart.java
@@ -71,9 +71,7 @@ public class AppStart {
     private static String typeText(BufferedReader reader) {
         String text;
         StringBuilder textSB = new StringBuilder();
-        System.out.println("=================================================================================================================================");
-        System.out.println("                              PLEASE ENTER YOUR TEXT HERE (type \"end\" on a new line to stop)");
-        System.out.println("=================================================================================================================================");
+        System.out.println("--> Please enter your text here (type \"end\" on a new line to stop):");
         try {
             while (true) {
                 if ("end".equals(text = reader.readLine())) break;
@@ -85,7 +83,6 @@ public class AppStart {
         } catch (IOException e) {
             System.err.println("--> Input error!");
         }
-        System.out.println("=================================================================================================================================");
         return textSB.toString();
     }
 
@@ -97,11 +94,11 @@ public class AppStart {
         System.out.println("cd [folder_name]             - goes to folder [folder_name] from current directory, use command \"..\" or \"cd..\" to go up one level;");
         System.out.println("mkdir [folder_name]          - creates a folder [folder_name] in current directory;");
         System.out.println("create [file_name.extension] - creates a [file_name.extension] file in current directory;");
-        System.out.println("delete [file_name.extension] - deletes a [file_name.extension] file from current directory;");
+        System.out.println("delete [name]                - deletes a [file_name.extension] or a [folder_name] file/folder from current directory;");
         System.out.println("setdir [directory]           - sets current directory to [directory];");
         System.out.println("open [file_name.extension]   - opens file [file_name.extension] from current directory;");
-        System.out.println("rewrite [file_name]          - rewrites a .txt [file_name] with the text you enter;");
-        System.out.println("addto [file_name]            - adds to a .txt [file_name] text you enter;");
+        System.out.println("rewrite [file_name]          - rewrites a [file_name] file with the text you enter;");
+        System.out.println("addto [file_name]            - adds to a [file_name] file text you enter;");
         System.out.println("help or ?                    - show list of all supported commands;");
         System.out.println("exit or quit                 - exit the program.");
         System.out.println("=================================================================================================================================");
